44b210d91eb139062079cd8ab2dd4f8ed042680f
Paolo <p.fiaschi@hotmail.it>
2020-03-23 18:03:10 +0100

    Esercizio completato

--- mail/main.js
+++ mail/main.js
@@ -8,7 +8,7 @@ var emailList = ["mario.rossi@gmail.com", "m.rossi@gmail.com", "m.r.@gmail.com",
 var emailUtente = prompt("Inserisci la tua email");
 
 // var email check
-var emailTrovata;
+var emailTrovata = "L'Utente: " + emailUtente + " non si è ancora registrato";
 
 
 console.log(emailList);
@@ -21,36 +21,12 @@ for (var i = 0; i < emailList.length; i++){
   console.log(emailList[i]);
 
   if (emailList[i] == emailUtente) {
-    i > emailList.length;
-    emailTrovata = emailUtente + " si";
+    emailTrovata = "L'Utente: " + emailUtente + " si è già registrato correttamente";
   }
-  // else {
-  //  emailTrovata = emailUtente + " errore";
-  // }
 console.log(emailUtente == emailList[i]);
 
 }
 
-// for (var j = 0; j < emailList.length; i++){
-//
-//   console.log(emailList[j]);
-//
-//   if (emailList[j] !== emailUtente) {
-//     emailTrovata = emailUtente + " errore";
-//   }
-//   // else {
-//   //  emailTrovata = emailUtente + " errore";
-//   // }
-// console.log(emailUtente !== emailList[j]);
-//
-// }
-
-
-
-
-// if (emailList[i] !== emailUtente){
-//   emailTrovata = emailUtente + " errore";
-//  }
 
 // output emailCheck se presente nella lista
 document.getElementById('mail').innerHTML = emailTrovata;
